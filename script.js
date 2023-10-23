8942dbf36b7993c320391c14940b442fea732be1
Érica Santos <104101478+theerica28@users.noreply.github.com>
2023-10-23 00:01:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Sucesso')
+const offset = 0
+const limit = 10
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+
+
+fetch(url)
+.then((response) => response.json())
+.then((jsonBody) => console.log(jsonBody))
+.catch((error) => console.error(error))
+
+
+.catch(function(error) {
+
+    console.error(error)
+})
+
+.finally(function (){
+    console.log('Requisição concluída')
+
+})
+
